04960dd262ce45275057c648dba9d244cefcbba5
smatei16 <55893625+smatei16@users.noreply.github.com>
2024-03-30 17:57:58 +0200

    [TH-7] Added allowed CORS origins

--- src/main/java/com/treasury/treasuryhub/controller/UserController.java
+++ src/main/java/com/treasury/treasuryhub/controller/UserController.java
@@ -11,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:3000", "https://treasury-hub-a7870221c88d.herokuapp.com/"})
+//@CrossOrigin(origins = {"http://localhost:3000", "https://treasury-hub-a7870221c88d.herokuapp.com/"})
+@CrossOrigin
 public class UserController {
 
     @Autowired
